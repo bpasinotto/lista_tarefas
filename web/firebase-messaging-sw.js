c32aec32ca5a02a64accdd2b625bf2fe702f6d01
Bruno <bruno_pasinotto@hotmail.com>
2025-08-02 00:22:28 -0300

    melhoria SW

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -8,6 +8,9 @@ self.addEventListener('activate', function (event) {
     event.waitUntil(self.clients.claim());
 });
 
+// Armazenar referências dos timeouts para poder cancelá-los
+let scheduledTimeouts = [];
+
 self.addEventListener('message', function (event) {
     console.log('Mensagem recebida no SW:', event.data);
 
@@ -16,24 +19,46 @@ self.addEventListener('message', function (event) {
 
         console.log('Agendando notificação para', delay, 'ms');
 
-        setTimeout(() => {
+        // Cancelar notificações anteriores
+        scheduledTimeouts.forEach(timeout => clearTimeout(timeout));
+        scheduledTimeouts = [];
+
+        // Agendar nova notificação
+        const timeoutId = setTimeout(() => {
             self.registration.showNotification(title, {
                 body: body,
                 icon: '/icons/Icon-192.png',
                 badge: '/icons/Icon-192.png',
                 tag: 'tarefas-reminder',
-                requireInteraction: false,
-                silent: false
+                requireInteraction: true,
+                silent: false,
+                vibrate: [200, 100, 200],
+                actions: [
+                    {
+                        action: 'view',
+                        title: 'Ver Tarefas'
+                    },
+                    {
+                        action: 'dismiss',
+                        title: 'Dispensar'
+                    }
+                ]
             });
+            
+            console.log('Notificação mostrada:', title);
         }, delay);
+
+        scheduledTimeouts.push(timeoutId);
     }
 });
 
 self.addEventListener('notificationclick', function (event) {
-    console.log('Notificação clicada');
+    console.log('Notificação clicada:', event.action);
     event.notification.close();
 
-    event.waitUntil(
-        clients.openWindow('/')
-    );
+    if (event.action === 'view' || !event.action) {
+        event.waitUntil(
+            clients.openWindow('/')
+        );
+    }
 });
